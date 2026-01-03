da60d15c721dc926dbaa875903c0e0daef038abd
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-01-03 14:14:24 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Hero from "./sections/Hero.jsx"
+export default function App() {
+    return (
+        <>
+            <Hero />
+        </>
+    )
+}
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,43 @@
+import { words } from "../constants/index.js";
+import Button from "../components/Button.jsx";
+const Herox = () => {
+  return (
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="background" />
+      </div>
+      <div className="hero-layout">
+        <header className="flex flex-col justify-center w-full px-10 md:px-20">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>Shaping
+                <span className="slide">
+                  <span className="wrapper" >
+                    {words.map((word) => (
+                      <span key={word.text} className="flex items-center md:gap-3 gap-1 p-2">
+                        <img src={word.imgPath} alt={word.text} className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50" />
+                        {word.text}
+                      </span>
+                    ))}
+                  </span>
+
+                </span>
+
+              </h1>
+              <h1>into Real Projects</h1>
+              <h1>that deliver Results</h1>
+            </div>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none"> Hi I'm Ahmed, a developer from Pakistan with a passion for creating innovative and efficient web solutions. </p>
+            <Button
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="button"
+              text="See my work"
+            />
+          </div>
+        </header>
+      </div>
+    </section>
+  )
+}
+
+export default Herox
