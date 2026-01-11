ec195becc9cd333bc99e411361604e560dc0cb8d
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-01-11 13:05:01 +0500

    Completed the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import { words } from "../constants/index.js";
 import Button from "../components/Button.jsx";
+import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
 const Herox = () => {
   return (
     <section id="hero" className="relative overflow-hidden">
@@ -13,8 +14,8 @@ const Herox = () => {
               <h1>Shaping
                 <span className="slide">
                   <span className="wrapper" >
-                    {words.map((word) => (
-                      <span key={word.text} className="flex items-center md:gap-3 gap-1 p-2">
+                    {words.map((word, index) => (
+                      <span key={`${word.text}-${index}`} className="flex items-center md:gap-3 gap-1 p-2">
                         <img src={word.imgPath} alt={word.text} className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50" />
                         {word.text}
                       </span>
@@ -27,7 +28,7 @@ const Herox = () => {
               <h1>into Real Projects</h1>
               <h1>that deliver Results</h1>
             </div>
-            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none"> Hi I'm Ahmed, a developer from Pakistan with a passion for creating innovative and efficient web solutions. </p>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none"> Hi I'm Ahmed, a developer from Pakistan <br /> with a passion for creating innovative and efficient web solutions. </p>
             <Button
               className="md:w-80 md:h-16 w-60 h-12"
               id="button"
@@ -35,6 +36,11 @@ const Herox = () => {
             />
           </div>
         </header>
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
       </div>
     </section>
   )
